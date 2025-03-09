7f371f71b3463cc3304f848f9c3d4ccfe0de151c
Shamathmika <vshamathmika@gmail.com>
2025-03-09 00:50:01 -0800

    Backend
    Added backened setup with folder structure

---
+++ OpenTable/src/main/java/com/_2/OpenTable/OpenTableApplication.java
@@ -0,0 +1,13 @@
+package com._2.OpenTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenTableApplication.class, args);
+	}
+
+}
