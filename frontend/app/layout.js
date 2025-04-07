c56b00175c20a93017dea14ceee28ec145371d34
Shamathmika <vshamathmika@gmail.com>
2025-04-07 09:54:40 -0700

    suppressed hydration

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -8,6 +8,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={` antialiased`}
+        suppressHydrationWarning
       >
 
         {children}
